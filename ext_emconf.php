68dc40281272525f73599275eb5d19521b1cb655
Gregor Agnes <ga@zwo3.de>
2020-01-24 13:22:08 +0100

    initial Checkin

---
+++ ext_emconf.php
@@ -0,0 +1,31 @@
+<?php
+
+$EM_CONF['newsletter_subscribe'] = array(
+	'title' => 'TYPO3 One Time Secret',
+	'description' => 'Send one time link for sensitive information with time limitation',
+	'category' => 'plugin',
+	'author' => 'Gregor Agnes',
+	'author_email' => 'ga@zwo3.de',
+	'author_company' => 'Gregor Agnes & Markus Cousin GbR',
+	'shy' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'beta',
+	'internal' => '',
+	'uploadfolder' => '1',
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'version' => '0.0.1',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '9.5.0-9.5.99',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+);
+
